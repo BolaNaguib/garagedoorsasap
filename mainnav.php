f5aeab4d14b883065164e3c296e5a911f9a37dcd
bola naguib <bolanaguib@gmail.com>
2019-05-01 04:46:42 +0200

    ddddddwww

--- mainnav.php
+++ mainnav.php
@@ -6,22 +6,22 @@
                         <!-- Top Contact -->
                         <div class="top-contact link-hover-black">
                         <a href="tel:+18779197828">
-                        <i class="fa fa-phone"></i>(877) 919-7828</a> 
+                        <i class="fa fa-phone"></i>(877) 919-7828</a>
                         <a href="mailto:garagedoorasapservice@gmail.com">
                         <i class="fa fa-envelope"></i>garagedoorasapservice@gmail.com</a></div>
                         <!-- Top Social Icon -->
                         <div class="top-social-icon icons-hover-black">
                         <a href="https://www.facebook.com/Garagedoorasapservice/" target="_blank">
                             <i class="fab fa-facebook-f"></i>
-                        </a>  
+                        </a>
                         <a href="https://www.youtube.com/channel/UCruZpdtFdDZq0obavLzPdyA" target="_blank">
                             <i class="fab fa-youtube"></i>
-                        </a> 
+                        </a>
                         <a href="https://goo.gl/maps/N6nmFKD27U82" target="_blank">
                             <i class="fas fa-map-marker-alt"></i>
-                        </a> 
+                        </a>
                            <a href="#usa" >
-<img src="img/social/usa.png">                        </a> 
+<img src="img/social/usa.png">                        </a>
                     </div>
                 </div>
             </div>
@@ -36,13 +36,13 @@
                                 <div class="navbar-header">
                                 <!-- Button For Responsive toggle -->
                                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                                <span class="sr-only">Toggle navigation</span> 
-                                <span class="icon-bar"></span> 
-                                <span class="icon-bar"></span> 
-                                <span class="icon-bar"></span></button> 
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span></button>
                                 <!-- Logo -->
-                                 
-                                <a class="navbar-brand" href="http://garagedoorasapservice.com/">
+
+                                <a class="navbar-brand" href="https://www.garagedoorasapservice.com/">
                                     <img class="site_logo" alt="Site Logo" src="img/logo.png" />
                                 </a></div>
                                 <!-- Navbar Collapse -->
@@ -51,7 +51,7 @@
                                     <ul class="nav navbar-nav">
                                         <!-- Home  Mega Menu -->
                                         <li>
-                                            <a href="http://garagedoorasapservice.com/">Home</a>
+                                            <a href="https://www.garagedoorasapservice.com/">Home</a>
                                         </li>
 										<li>
                                             <a href="about/">About</a>
@@ -60,21 +60,21 @@
                                             <a class="active" >Services</a>
                                              <ul class="dropdown-menu">
                                                <li>
-<a href="http://garagedoorasapservice.com/General-Maintnance/">Garage Door General Maintenance</a></li><li>
-<a href="http://garagedoorasapservice.com/Garage-Door-Spring-Replacment/">Garage Door Spring Replacement</a></li><li>
-<a href="http://garagedoorasapservice.com/Garage-Door-Opener-Repair/">Garage Door Opener Repair</a></li><li>
-<a href="http://garagedoorasapservice.com/Garage-door-opener-replacment/">Garage Door Opener Replacement</a></li><li>
-<a href="http://garagedoorasapservice.com/Garage-Door-Roller-Replacment/">Garage Door Roller Replacement</a></li><li>
-<a href="http://garagedoorasapservice.com/Garage-Door-Shifted-Door-Repair/">Shifted Garage Door Repair</a></li>
+<a href="https://www.garagedoorasapservice.com/General-Maintnance/">Garage Door General Maintenance</a></li><li>
+<a href="https://www.garagedoorasapservice.com/Garage-Door-Spring-Replacment/">Garage Door Spring Replacement</a></li><li>
+<a href="https://www.garagedoorasapservice.com/Garage-Door-Opener-Repair/">Garage Door Opener Repair</a></li><li>
+<a href="https://www.garagedoorasapservice.com/Garage-door-opener-replacment/">Garage Door Opener Replacement</a></li><li>
+<a href="https://www.garagedoorasapservice.com/Garage-Door-Roller-Replacment/">Garage Door Roller Replacement</a></li><li>
+<a href="https://www.garagedoorasapservice.com/Garage-Door-Shifted-Door-Repair/">Shifted Garage Door Repair</a></li>
                                             </ul>
                                         </li>
 									
 										<li>
-                                            <a href="http://garagedoorasapservice.com/testimonials/">Reviews</a>
+                                            <a href="https://www.garagedoorasapservice.com/testimonials/">Reviews</a>
                                         </li>
                                         <!-- Contact Block -->
                                         <li>
-                                            <a href="http://garagedoorasapservice.com/contact/">Contact</a>
+                                            <a href="https://www.garagedoorasapservice.com/contact/">Contact</a>
                                         </li>
                                         <!-- Ends Contact Block -->
 										<!-- Features Menu -->
@@ -90,21 +90,21 @@
                                                             <h6 class="title">Cities we serve</h6>
                                                             <!-- Links -->
                                                             <div class="page-links">
-                                                               
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Orlando_Fl/"> Orlando </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Kissimmee_Fl/"> Kissimmee </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_The_Villages_Fl/"> The Villages </a></div>
-<div><a href="http://garagedoorasapservice.com/clermont/"> Clermont </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Apopka_Fl/"> Apopka </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Winter_Garden_Fl/"> Winter Garden </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Winter_Park_Fl/"> Winter Park </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Sanford_Fl/"> Sanford </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Casselberry_Fl/"> Casselberry </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Oviedo_Fl/"> Oviedo </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Lake_Mary_Fl/"> Lake Mary </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Altamonte_Springs_Fl/"> Altamonte Springs </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Longwood_Fl/"> Longwood </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Windermere_Fl/"> Windermere </a></div>
+
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Orlando_Fl/"> Orlando </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Kissimmee_Fl/"> Kissimmee </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_The_Villages_Fl/"> The Villages </a></div>
+<div><a href="https://www.garagedoorasapservice.com/clermont/"> Clermont </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Apopka_Fl/"> Apopka </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Winter_Garden_Fl/"> Winter Garden </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Winter_Park_Fl/"> Winter Park </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Sanford_Fl/"> Sanford </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Casselberry_Fl/"> Casselberry </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Oviedo_Fl/"> Oviedo </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Lake_Mary_Fl/"> Lake Mary </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Altamonte_Springs_Fl/"> Altamonte Springs </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Longwood_Fl/"> Longwood </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Windermere_Fl/"> Windermere </a></div>
 
 
                                                             </div>
@@ -115,24 +115,24 @@
                                                             <h6 class="title"></h6>
                                                             <!-- Links -->
                                                             <div class="page-links">
-                                                               
 
 
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Leesburg_Fl/"> Leesburg </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Ocoee_Fl/"> Ocoee </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Pine_Hills_Fl/"> Pine Hills </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Groveland_Fl/"> Groveland </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Eustis_Fl/"> Eustis </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Maitland_Fl/"> Maitland </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Wildwood_Fl/"> Wildwood </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Minneola_Fl/"> Minneola </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Mount_Dora_Fl/"> Mount Dora </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Tavares_Fl/"> Tavares </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Lockhart_Fl/"> Lockhart </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Saint_Cloud_Fl/"> Saint Cloud </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Winter_Springs_Fl/"> Winter Springs </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Coleman_Fl/"> Coleman </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Umatilla_Fl/"> Umatilla </a></div>
+
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Leesburg_Fl/"> Leesburg </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Ocoee_Fl/"> Ocoee </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Pine_Hills_Fl/"> Pine Hills </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Groveland_Fl/"> Groveland </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Eustis_Fl/"> Eustis </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Maitland_Fl/"> Maitland </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Wildwood_Fl/"> Wildwood </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Minneola_Fl/"> Minneola </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Mount_Dora_Fl/"> Mount Dora </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Tavares_Fl/"> Tavares </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Lockhart_Fl/"> Lockhart </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Saint_Cloud_Fl/"> Saint Cloud </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Winter_Springs_Fl/"> Winter Springs </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Coleman_Fl/"> Coleman </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Umatilla_Fl/"> Umatilla </a></div>
 
 
                                                             </div>
@@ -143,20 +143,20 @@
                                                             <h6 class="title"></h6>
                                                             <!-- Links -->
                                                             <div class="page-links">
-                                       
 
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Fruitland_Park_Fl/"> Fruitland Park </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Lady_Lake_Fl/"> Lady Lake </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Bushnell_Fl/"> Bushnell </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Mascotte_Fl/"> Mascotte </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Grand_Island_Fl/"> Grand Island </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Webster_Fl/"> Webster </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Montverde_Fl/"> Montverde </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Astatula_Fl/"> Astatula </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Fairview_Shores_Fl/"> Fairview Shores </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_CenterHill_Fl/"> CenterHill </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Oakland_Fl/"> Oakland </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Gotha_Fl/"> Gotha </a></div>
+
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Fruitland_Park_Fl/"> Fruitland Park </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Lady_Lake_Fl/"> Lady Lake </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Bushnell_Fl/"> Bushnell </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Mascotte_Fl/"> Mascotte </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Grand_Island_Fl/"> Grand Island </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Webster_Fl/"> Webster </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Montverde_Fl/"> Montverde </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Astatula_Fl/"> Astatula </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Fairview_Shores_Fl/"> Fairview Shores </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_CenterHill_Fl/"> CenterHill </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Oakland_Fl/"> Oakland </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Gotha_Fl/"> Gotha </a></div>
 
                                                             </div>
                                                         </div>
@@ -166,15 +166,15 @@
                                                             <h6 class="title"> Counties we serve</h6>
                                                             <!-- Links -->
                                                             <div class="page-links">
-                                       
 
 
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Orange_County_Fl/"> Orange County, Florida </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Seminole_County_Fl/"> Seminole County, Florida </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Osceola_County_Fl/"> Osceola County, Florida </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Lake_County_Fl/"> Lake County, Florida </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Volusia_County_Fl/"> Volusia County, Florida </a></div>
-<div><a href="http://garagedoorasapservice.com/garage_door_repair_Pinellas_County_Fl/"> Pinellas County, Florida </a></div>
+
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Orange_County_Fl/"> Orange County, Florida </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Seminole_County_Fl/"> Seminole County, Florida </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Osceola_County_Fl/"> Osceola County, Florida </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Lake_County_Fl/"> Lake County, Florida </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Volusia_County_Fl/"> Volusia County, Florida </a></div>
+<div><a href="https://www.garagedoorasapservice.com/garage_door_repair_Pinellas_County_Fl/"> Pinellas County, Florida </a></div>
                                                             </div>
                                                         </div>
                                                     </div>
