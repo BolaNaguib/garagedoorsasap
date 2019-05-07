906af9c49b66407ed56617d27272e5489a6334f7
bola naguib <bolanaguib@gmail.com>
2019-05-07 13:10:20 +0200

    updated rating

--- lpfooterafter.php
+++ lpfooterafter.php
@@ -9,59 +9,59 @@
         <h3> <b> Cities we serve</b></h3>
         <hr>
         <ul>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Orlando_Fl/"> Orlando </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Kissimmee_Fl/"> Kissimmee </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_The_Villages_Fl/"> The Villages </a></li>
-          <li><a href="http://garagedoorasapservice.com/Clermont/"> Clermont </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Apopka_Fl/"> Apopka </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Winter_Garden_Fl/"> Winter Garden </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Winter_Park_Fl/"> Winter Park </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Sanford_Fl/"> Sanford </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Casselberry_Fl/"> Casselberry </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Oviedo_Fl/"> Oviedo </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Lake_Mary_Fl/"> Lake Mary </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Altamonte_Springs_Fl/"> Altamonte Springs </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Longwood_Fl/"> Longwood </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Windermere_Fl/"> Windermere </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Leesburg_Fl/"> Leesburg </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Ocoee_Fl/"> Ocoee </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Pine_Hills_Fl/"> Pine Hills </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Groveland_Fl/"> Groveland </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Eustis_Fl/"> Eustis </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Maitland_Fl/"> Maitland </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Wildwood_Fl/"> Wildwood </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Minneola_Fl/"> Minneola </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Mount_Dora_Fl/"> Mount Dora </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Tavares_Fl/"> Tavares </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Lockhart_Fl/"> Lockhart </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Saint_Cloud_Fl/"> Saint Cloud </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Winter_Springs_Fl/"> Winter Springs </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Coleman_Fl/"> Coleman </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Umatilla_Fl/"> Umatilla </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Fruitland_Park_Fl/"> Fruitland Park </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Lady_Lake_Fl/"> Lady Lake </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Bushnell_Fl/"> Bushnell </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Mascotte_Fl/"> Mascotte </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Grand_Island_Fl/"> Grand Island </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Webster_Fl/"> Webster </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Montverde_Fl/"> Montverde </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Astatula_Fl/"> Astatula </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Fairview_Shores_Fl/"> Fairview Shores </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_CenterHill_Fl/"> CenterHill </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Oakland_Fl/"> Oakland </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Gotha_Fl/"> Gotha </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Orlando_Fl/"> Orlando </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Kissimmee_Fl/"> Kissimmee </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_The_Villages_Fl/"> The Villages </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/Clermont/"> Clermont </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Apopka_Fl/"> Apopka </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Winter_Garden_Fl/"> Winter Garden </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Winter_Park_Fl/"> Winter Park </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Sanford_Fl/"> Sanford </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Casselberry_Fl/"> Casselberry </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Oviedo_Fl/"> Oviedo </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Lake_Mary_Fl/"> Lake Mary </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Altamonte_Springs_Fl/"> Altamonte Springs </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Longwood_Fl/"> Longwood </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Windermere_Fl/"> Windermere </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Leesburg_Fl/"> Leesburg </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Ocoee_Fl/"> Ocoee </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Pine_Hills_Fl/"> Pine Hills </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Groveland_Fl/"> Groveland </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Eustis_Fl/"> Eustis </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Maitland_Fl/"> Maitland </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Wildwood_Fl/"> Wildwood </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Minneola_Fl/"> Minneola </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Mount_Dora_Fl/"> Mount Dora </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Tavares_Fl/"> Tavares </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Lockhart_Fl/"> Lockhart </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Saint_Cloud_Fl/"> Saint Cloud </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Winter_Springs_Fl/"> Winter Springs </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Coleman_Fl/"> Coleman </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Umatilla_Fl/"> Umatilla </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Fruitland_Park_Fl/"> Fruitland Park </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Lady_Lake_Fl/"> Lady Lake </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Bushnell_Fl/"> Bushnell </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Mascotte_Fl/"> Mascotte </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Grand_Island_Fl/"> Grand Island </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Webster_Fl/"> Webster </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Montverde_Fl/"> Montverde </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Astatula_Fl/"> Astatula </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Fairview_Shores_Fl/"> Fairview Shores </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_CenterHill_Fl/"> CenterHill </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Oakland_Fl/"> Oakland </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Gotha_Fl/"> Gotha </a></li>
         </ul>
       </div>
       <div class="col-md-3">
         <h3> <b> Counties we serve</b></h3>
         <hr>
         <ul>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Orange_County_Fl/"> Orange County, Florida </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Seminole_County_Fl/"> Seminole County, Florida </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Osceola_County_Fl/"> Osceola County, Florida </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Lake_County_Fl/"> Lake County, Florida </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Volusia_County_Fl/"> Volusia County, Florida </a></li>
-          <li><a href="http://garagedoorasapservice.com/garage_door_repair_Pinellas_County_Fl/"> Pinellas County, Florida </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Orange_County_Fl/"> Orange County, Florida </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Seminole_County_Fl/"> Seminole County, Florida </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Osceola_County_Fl/"> Osceola County, Florida </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Lake_County_Fl/"> Lake County, Florida </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Volusia_County_Fl/"> Volusia County, Florida </a></li>
+          <li><a href="https://www.garagedoorasapservice.com/garage_door_repair_Pinellas_County_Fl/"> Pinellas County, Florida </a></li>
         </ul>
       </div>
     </div>
@@ -82,13 +82,17 @@
     <div class="col-md-6">
       <!-- <span style="color:#ffc600" class="rating-desc" itemscope="" itemtype="http://schema.org/Product">
 <span style="color:#ffc600" itemprop="name">Julie Azcano</span>
-<span style="color:#ffc600" itemprop="aggregateRating" itemscope="" itemtype="http://schema.org/AggregateRating"> Rated <span itemprop="ratingValue">4.9</span> / 5 based on <span itemprop="reviewCount">5</span> reviews. | </span>
+<span style="color:#ffc600" itemprop="aggregateRating" itemscope="" itemtype="http://schema.org/AggregateRating"> Rated
+<span itemprop="ratingValue">4.9</span> / 5 based on
+<span itemprop="reviewCount">5</span> reviews. | </span>
 </span> -->
     </div>
     <div class="col-md-6">
-      <span style="color:#ffc600"  class="rating-desc" itemscope="" itemtype="http://schema.org/Product">
-<span  itemprop="aggregateRating" itemscope="" itemtype="http://schema.org/AggregateRating" >Rated</span>
-         <span itemprop="ratingValue">4.9</span>/148 Based On <span itemprop="reviewCount">148</span>
+        <span style="color:#ffc600"  class="rating-desc" itemscope="" itemtype="http://schema.org/Product">
+          <span style="color:#ffc600" itemprop="name"></span>
+        <span  itemprop="aggregateRating" itemscope="" itemtype="http://schema.org/AggregateRating" >Rated</span>
+         <span itemprop="ratingValue">4.9</span>/148 Based On
+         <span itemprop="reviewCount">148</span>
          <a class="ratings"
          href="https://www.google.com/maps/place/Garage+Door+ASAP+Service/@28.6043929,-81.7609423,17z/data=!4m7!3m6!1s0x88e7894ff1fd8cc3:0x4bd696395a30e80b!8m2!3d28.6043929!4d-81.7587483!9m1!1b1">Google Reviews</a>
 
